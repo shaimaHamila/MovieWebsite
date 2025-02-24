ba011744cfce00634a80259bf65dfdb8a2c0647f
shaimaHamila <hamilachaima18@gmail.com>
2025-02-24 10:51:13 +0300

    configure tailwind css

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -1,4 +1,9 @@
 const Home: React.FC = () => {
-  return <h1>Home</h1>;
+  return (
+    <div>
+      <h1 className='text-3xl font-bold underline'>Hello world! Home page</h1>
+      <h1 className='text-red-500'>Hello Tailwind</h1>
+    </div>
+  );
 };
 export default Home;
