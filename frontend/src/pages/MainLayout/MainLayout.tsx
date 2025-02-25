7163ab72f20b2eb3cf40a2960cf7ee001329001d
shaimaHamila <hamilachaima18@gmail.com>
2025-02-25 10:09:21 +0300

    finsh header design integration

--- frontend/src/pages/MainLayout/MainLayout.tsx
+++ frontend/src/pages/MainLayout/MainLayout.tsx
@@ -1,10 +1,10 @@
 import { Outlet } from "react-router-dom";
 import Footer from "../../components/organisms/Footer/Footer";
 import Header from "../../components/organisms/Header/Header";
-
+import "./MainLayout.css";
 const MainLayout: React.FC = () => {
   return (
-    <div>
+    <div className='main_layout-container text-white  h-screen bg-neutral-900'>
       <Header />
       <Outlet />
       <Footer />
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,5 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: "class",
+  theme: "dark",
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
